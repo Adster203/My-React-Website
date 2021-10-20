0c18c376da936b6abda9cc47961a98a8d6f651e1
Amin Akbar <aditta.203@gmail.com>
2021-10-20 15:09:07 +0600

    topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,15 +1,31 @@
 import './topbar.min.css';
+import PersonIcon from '@mui/icons-material/Person';
+import MailIcon from '@mui/icons-material/Mail';
 
-export default function topbar() {
+export default function topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className='topbar'>
+    <div className={`topbar ${menuOpen && 'active'}`}>
       <div className='wrapper'>
         <div className='left'>
           <a href='#intro' className='logo'>
-            genius.
+            Amin Akbar
           </a>
+          <div className='itemContainer'>
+            <PersonIcon className='icon' />
+            <span>01672207555</span>
+          </div>
+          <div className='itemContainer'>
+            <MailIcon className='icon' />
+            <span>aditta.203@gmail.com</span>
+          </div>
         </div>
-        <div className='right'></div>
+        <div className='right'>
+          <div className='hamburger' onClick={() => setMenuOpen(!menuOpen)}>
+            <span className='line1'></span>
+            <span className='line2'></span>
+            <span className='line3'></span>
+          </div>
+        </div>
       </div>
     </div>
   );
